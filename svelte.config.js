889473292b6659c2fb01157d0380f5bc7482d3ff
Nea Tiihonen <ah2393@student.jamk.fi>
2026-03-05 16:03:07 +0200

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		// default options are shown
 		adapter: adapter({
-			// see below for options that can be set here
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
 		})
 	}
 };
